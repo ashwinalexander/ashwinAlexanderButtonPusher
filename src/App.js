e6a6baa7a92d44b4e84aaa4f45483da27e5037b9
ashwin <alexwin75@gmail.com>
2020-08-08 20:53:54 -0400

    initial setup :rocket:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    }
+  }
+
+
+  componentDidMount() {
+    // const dbRef = firebase.database().ref();
+
+    // dbRef.on('value', (snapshot) => {
+    //   const data = snapshot.val();
+
+    //   const newBooksArray = [];
+    //   for (let propertyName in data) {
+    //     newBooksArray.push(data[propertyName]);
+    //   }
+    //   console.log(newBooksArray);
+
+    //   this.setState({
+    //     books: newBooksArray
+    //   })
+    // })
+
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>look a boookshelf</h1>
+        {/* use a map instead of a foreach because the map RETURNS an array */}
+        <ul>
+          {
+            this.state.books.map((book) => {
+              return <li>{book}</li>
+            })
+          }
+        </ul>
+
+
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
