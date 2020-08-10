7e5576387960466637a797dee73d78d5caa364fa
ashwin <alexwin75@gmail.com>
2020-08-10 16:42:04 -0400

    Updates

---
+++ precache-manifest.05fdb6ea4d37c123ed56a362e318c124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5528de09c847418631cf7158511176cb",
+    "url": "/ashwinAlexanderButtonPusher/index.html"
+  },
+  {
+    "revision": "de64bba5bdd52c95a87e",
+    "url": "/ashwinAlexanderButtonPusher/static/css/main.48f05698.chunk.css"
+  },
+  {
+    "revision": "05820b784239662bcdc3",
+    "url": "/ashwinAlexanderButtonPusher/static/js/2.9d9683b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ashwinAlexanderButtonPusher/static/js/2.9d9683b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de64bba5bdd52c95a87e",
+    "url": "/ashwinAlexanderButtonPusher/static/js/main.07cf54a6.chunk.js"
+  },
+  {
+    "revision": "87e31e7da0b0358fcc80",
+    "url": "/ashwinAlexanderButtonPusher/static/js/runtime-main.61bcc9ee.js"
+  }
+]);
